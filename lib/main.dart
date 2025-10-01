e95808cd4312a02301cdec687c3e352dc00dbd41
ArwaElsherif <arwa01273648207@gmail.com>
2025-10-01 19:47:43 +0300

    add kPrimaryColor

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
@@ -5,13 +6,18 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 void main() {
   runApp(const BooklyApp());
 }
+
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
       home: SplashView(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
