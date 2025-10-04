156a0802eb423de5fe56bf7c9d7335cc8c14b7ce
ArwaElsherif <arwa01273648207@gmail.com>
2025-10-04 12:06:50 +0300

    handel folder

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,5 +1,5 @@
-import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/core/constants/constants.dart';
+import 'package:bookly_app/core/constants/assets.dart';
 import 'package:bookly_app/features/home/presentation/view/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text_widget.dart';
 import 'package:flutter/material.dart';
