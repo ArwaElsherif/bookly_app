c694581f9f002bbe91308726146286f9941a9d8e
ArwaElsherif <arwa01273648207@gmail.com>
2025-10-14 23:07:34 +0300

    add similar list

--- lib/features/home/presentation/view/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/view/widgets/book_details_view_body.dart
@@ -4,10 +4,12 @@ import 'package:bookly_app/features/home/presentation/view/widgets/books_details
 import 'package:bookly_app/features/home/presentation/view/widgets/custom_book_details_appbar.dart';
 import 'package:bookly_app/features/home/presentation/view/widgets/similar_hirozontal_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import '../../../../../core/theme/text_styles.dart';
 import '../../../data/models/book_model.dart';
+import '../../view_model/cubits/all_books_cubits/all_books_cubit.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -18,7 +20,6 @@ class BookDetailsViewBody extends StatelessWidget {
     if (book == null) {
       return const Scaffold(body: Center(child: Text('No book data')));
     }
-
     return SingleChildScrollView(
       child: Column(
         children: [
@@ -37,9 +38,7 @@ class BookDetailsViewBody extends StatelessWidget {
                 ),
                 Padding(
                   padding: EdgeInsets.symmetric(vertical: 28.w),
-                  child:  BooksAction(
-                    price: book.price,
-                  ),
+                  child: BooksAction(price: book.price),
                 ),
                 Align(
                   alignment: Alignment.bottomLeft,
@@ -51,7 +50,23 @@ class BookDetailsViewBody extends StatelessWidget {
               ],
             ),
           ),
-          SimilarHirozontalList(),
+          BlocBuilder<AllBooksCubit, AllBooksState>(
+            builder: (context, state) {
+              if (state is AllBooksLoaded) {
+                final similarBooks = List<Book>.from(state.books)..shuffle();
+                similarBooks.removeWhere((b) => b.id == book.id);
+                final limitedSimilarBooks = similarBooks.take(5).toList();
+                return SimilarHirozontalList(books: limitedSimilarBooks);
+              } else if (state is AllBooksLoading) {
+                return const Center(child: CircularProgressIndicator());
+              } else {
+                return const SizedBox(
+                  height: 100,
+                  child: Center(child: Text('Failed to load similar books')),
+                );
+              }
+            },
+          ),
         ],
       ),
     );
--- lib/features/home/presentation/view/widgets/similar_hirozontal_list.dart
+++ lib/features/home/presentation/view/widgets/similar_hirozontal_list.dart
@@ -1,30 +1,32 @@
-import 'package:bookly_app/features/home/presentation/view/widgets/custom_item.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:bookly_app/features/home/presentation/view/widgets/custom_item.dart';
+import '../../../data/models/book_model.dart';
 
 class SimilarHirozontalList extends StatelessWidget {
-  const SimilarHirozontalList({
-    super.key,
-  });
-
+  const SimilarHirozontalList({super.key, required this.books});
+  final List<Book> books;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        height: MediaQuery.of(context).size.height * 0.22,
-        child: Padding(
-          padding: EdgeInsets.only(top: 10,bottom: 30, left: 30),
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            physics: const BouncingScrollPhysics(),
-            itemCount: 10,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: EdgeInsets.only(right: 6),
-                child: CustomItem(aspectRatio: 2.7/ 4,),
-              );
-            },
-          ),
+      height: MediaQuery.of(context).size.height * 0.22,
+      child: Padding(
+        padding: EdgeInsets.only(top: 10, bottom: 30, left: 30),
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          physics: const BouncingScrollPhysics(),
+          itemCount: books.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: EdgeInsets.only(right: 6),
+              child: CustomItem(
+                aspectRatio: 2.7 / 4,
+                imageUrl: books[index].image,
+              ),
+            );
+          },
         ),
-      );
+      ),
+    );
   }
 }
-
